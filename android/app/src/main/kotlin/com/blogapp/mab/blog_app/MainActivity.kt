b96e2f4f71b9c657b924435883825bf05de96187
mohamedelbendary <mohamed.elbendary@mobidevlabs.com>
2024-05-02 13:46:43 +0300

    start new project

---
+++ android/app/src/main/kotlin/com/blogapp/mab/blog_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.blogapp.mab.blog_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Blog App",
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+      ),
+    );
+  }
+}
