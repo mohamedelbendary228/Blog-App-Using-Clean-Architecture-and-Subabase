743d0a923425f93fc030a5d4608970caa2d37501
mohamedelbendary <mohamed.elbendary@mobidevlabs.com>
2024-05-04 14:22:20 +0300

    Remove app secrets file from repo

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const String supabaseUrl = "https://vlxaoiqefgyevhmcuiey.supabase.co";
-  static const String anonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZseGFvaXFlZmd5ZXZobWN1aWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4MjAzMjYsImV4cCI6MjAzMDM5NjMyNn0.y5u7sxdgsJpgP63Q1Oqam0dbCxYWXSV1lIAloMNv0ZI";
-}
